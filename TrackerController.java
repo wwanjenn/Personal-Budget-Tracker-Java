73d019c06208fc35612babd1f5b954f9d8839eea
Wanjin Maglangit <wmaglangit1118@gmail.com>
2025-07-07 13:57:13 +0800

    inital expense

---
+++ TrackerController.java
@@ -0,0 +1,13 @@
+class TrackerController {
+    private Expense expenseModel;
+    private Income incomeModel;
+    private  TrackerView trackerView;
+
+    public TrackerController(expenseModel, incomeModel, trackerView){
+        this.expenseModel = expenseModel;
+        this.incomeModel = incomeModel;
+        this.trackerView = trackerView;
+    }
+
+        
+}
