6a912a4c30cb5a960f1bfb69ae6724b4363d4cb0
Wanjin Maglangit <wmaglangit1118@gmail.com>
2025-07-08 14:26:16 +0800

    added controller to main

--- TrackerApp.java
+++ TrackerApp.java
@@ -1,7 +1,8 @@
 class TrackerApp {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(()->{
-            new TrackerView();
+            TrackerView view = new TrackerView();
+            TrackerController controller = new TrackerController(view);
         });
     }
 }
